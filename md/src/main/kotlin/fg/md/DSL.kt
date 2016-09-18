8c1a369964730f4ae4615fa181ef37f9593a628c
freshlyground <freshlyground@fastmail.com>
2016-09-18 11:55:37 +0200

    Simplified md DSL

--- md/src/main/kotlin/fg/md/DSL.kt
+++ md/src/main/kotlin/fg/md/DSL.kt
@@ -1,6 +1,7 @@
 package fg.md
 
 import fg.elements.Element
+import fg.elements.initAndAppendNode
 import fg.md.card.MDCard
 import fg.md.card.MDCardAvatar
 import fg.md.card.MDCardContent
@@ -9,113 +10,20 @@ import fg.md.card.MDCardHeaderText
 import fg.md.card.MDCardImage
 import fg.md.card.MDCardSubtitle
 import fg.md.card.MDCardTitle
+import fg.md.drawer.MDDrawer
 
-fun Element.mdCard(init: MDCard.() -> Unit): MDCard {
-    val el = MDCard()
-    el.init()
-    appendChild(el)
-    return el
-}
-
-fun mdCard(init: MDCard.() -> Unit): MDCard {
-    val el = MDCard()
-    el.init()
-    return el
-}
-
-fun MDCard.title(init: MDCardTitle.() -> Unit): MDCardTitle {
-
-    val title = MDCardTitle()
-    title.init()
-    appendChild(title)
-    return title
-}
-
-fun MDCardContent.title(init: MDCardTitle.() -> Unit): MDCardTitle {
-
-    val title = MDCardTitle()
-    title.init()
-    appendChild(title)
-    return title
-}
-
-fun MDCard.subtitle(init: MDCardSubtitle.() -> Unit): MDCardSubtitle {
-
-    val el = MDCardSubtitle()
-    el.init()
-    appendChild(el)
-    return el
-}
-
-fun MDCard.header(init: MDCardHeader.() -> Unit): MDCardHeader {
-
-    val el = MDCardHeader()
-    el.init()
-    appendChild(el)
-    return el
-}
-
-fun MDCard.image(src: String, init: MDCardImage.() -> Unit): MDCardImage {
-
-    val el = MDCardImage(src)
-    el.init()
-    appendChild(el)
-    return el
-}
-
-fun MDCardHeader.text(init: MDCardHeaderText.() -> Unit): MDCardHeaderText {
-
-    val title = MDCardHeaderText()
-    title.init()
-    appendChild(title)
-    return title
-}
-
-fun MDCardHeader.title(init: MDCardTitle.() -> Unit): MDCardTitle {
-
-    val title = MDCardTitle()
-    title.init()
-    appendChild(title)
-    return title
-}
-
-fun MDCardHeader.subtitle(init: MDCardSubtitle.() -> Unit): MDCardSubtitle {
-
-    val el = MDCardSubtitle()
-    el.init()
-    appendChild(el)
-    return el
-}
-
-fun MDCardHeaderText.title(init: MDCardTitle.() -> Unit): MDCardTitle {
-
-    val title = MDCardTitle()
-    title.init()
-    appendChild(title)
-    return title
-}
-
-fun MDCardHeaderText.subtitle(init: MDCardSubtitle.() -> Unit): MDCardSubtitle {
-
-    val el = MDCardSubtitle()
-    el.init()
-    appendChild(el)
-    return el
-}
-
-fun MDCardHeader.avatar(src: String, init: MDCardAvatar.() -> Unit): MDCardAvatar {
-
-    val el = MDCardAvatar(src)
-    el.init()
-    appendChild(el)
-    return el
-}
-
-fun MDCard.content(init: MDCardContent.() -> Unit): MDCardContent {
-
-    val content = MDCardContent()
-    content.init()
-    appendChild(content)
-    return content
-}
+fun Element.mdDrawer(init: MDDrawer.() -> Unit) = initAndAppendNode(MDDrawer(), init)
+fun Element.mdCard(init: MDCard.() -> Unit) = initAndAppendNode(MDCard(), init)
+fun MDCard.title(init: MDCardTitle.() -> Unit) = initAndAppendNode(MDCardTitle(), init)
+fun MDCard.subtitle(init: MDCardSubtitle.() -> Unit) = initAndAppendNode(MDCardSubtitle(), init)
+fun MDCard.header(init: MDCardHeader.() -> Unit) = initAndAppendNode(MDCardHeader(), init)
+fun MDCard.image(src: String, init: MDCardImage.() -> Unit) = initAndAppendNode(MDCardImage(src), init)
+fun MDCard.content(init: MDCardContent.() -> Unit) = initAndAppendNode(MDCardContent(), init)
+fun MDCardHeader.text(init: MDCardHeaderText.() -> Unit) = initAndAppendNode(MDCardHeaderText(), init)
+fun MDCardHeader.title(init: MDCardTitle.() -> Unit) = initAndAppendNode(MDCardTitle(), init)
+fun MDCardHeader.subtitle(init: MDCardSubtitle.() -> Unit) = initAndAppendNode(MDCardSubtitle(), init)
+fun MDCardHeaderText.title(init: MDCardTitle.() -> Unit) = initAndAppendNode(MDCardTitle(), init)
+fun MDCardHeaderText.subtitle(init: MDCardSubtitle.() -> Unit) = initAndAppendNode(MDCardSubtitle(), init)
+fun MDCardHeader.avatar(src: String, init: MDCardAvatar.() -> Unit) = initAndAppendNode(MDCardAvatar(src), init)
+fun MDCardContent.title(init: MDCardTitle.() -> Unit) = initAndAppendNode(MDCardTitle(), init)
 
