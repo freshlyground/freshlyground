13123b10df4d2a99ab9e1fcd5d250a5b5a6353f9
freshlyground <freshlyground@fastmail.com>
2016-11-05 21:07:15 +0100

    Added ripple effect to MDMenuItem

--- material-design/src/main/kotlin/fg/md/menu/MDMenuItem.kt
+++ material-design/src/main/kotlin/fg/md/menu/MDMenuItem.kt
@@ -6,10 +6,23 @@ import fg.elements.HTML
 import fg.elements.StyledClass
 import fg.elements.toClassSelector
 import fg.md.pkg
+import fg.md.support.Ripple
 import fg.style.ClassRule
 import fg.style.colour.RgbColor
 
 class MDMenuItem(action: Action) : MenuItem(action) {
+
+    private val ripple: Ripple by lazy {
+        Ripple(element = this,
+                inkColor = RgbColor.BLUE,
+                startedCallback = {
+                    //this.ripples = true
+                },
+                doneCallback = {
+                    //this.ripples = false
+                    //renderColor()
+                })
+    }
 
     override fun render() {
         super.render()
@@ -17,6 +30,12 @@ class MDMenuItem(action: Action) : MenuItem(action) {
         addClass(MDMenuItem.classSelector)
 
         menuItemStyle.hovered.backgroundColor = RgbColor(153, 153, 153, 0.2)
+    }
+
+    override fun didMount() {
+        super.didMount()
+
+        ripple.init()
     }
 
     companion object MDMenuItem : StyledClass {
