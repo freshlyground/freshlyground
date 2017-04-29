81784053945cabde8bb587d3c5316892f0f5c1d7
freshlyground <freshlyground@fastmail.com>
2017-04-29 17:27:19 +0200

    Introduced extension function Element.collapseOn(SelectableAction)

--- beans/src/main/kotlin/fg/beans/package.kt
+++ beans/src/main/kotlin/fg/beans/package.kt
@@ -1,4 +1,21 @@
 package fg.beans
 
+import fg.beans.action.SelectableAction
+import fg.elements.Element
+
 val pkg: String = "fg-bn"
 
+fun Element.collapseOn(action: SelectableAction) {
+    action.onPropertyChanged({ _, property, _, _ ->
+        when (property.name) {
+            SelectableAction::selected.name -> {
+                if(action.selected) {
+                    this.show()
+                }
+                else {
+                    this.hide()
+                }
+            }
+        }
+    })
+}
