cf879a1ba5aacc2ce16030b37a19c5edf6c6c057
freshlyground <freshlyground@fastmail.com>
2017-09-18 18:50:06 +0200

    Introduced typealias ClassRuleBuilder

--- elements/src/main/kotlin/fg/elements/StyledClass.kt
+++ elements/src/main/kotlin/fg/elements/StyledClass.kt
@@ -2,9 +2,11 @@ package fg.elements
 
 import fg.style.ClassRule
 
+typealias ClassRuleBuilder = ClassRule.() -> Unit
+
 interface StyledClass {
 
     val classSelector: ClassSelector
 
-    val rule: ClassRule.() -> Unit
+    val rule: ClassRuleBuilder
 }
